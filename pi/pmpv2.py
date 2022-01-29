e636814e3db82d93cd004b42cae904fa3d10c2e1
Nathan Lu <nathanlumax@gmail.com>
2022-01-29 15:20:06 -0600

    brenan

--- pi/pmpv2.py
+++ pi/pmpv2.py
@@ -1,4 +1,15 @@
-import RPi.GPIO
+import RPi.GPIO as GPIO
+import time
 
 init = False
 
+def init_output(pin):
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, GPIO.LOW)
+    GPIO.output(pin, GPIO.HIGH)
+
+def pump_on(p_pin = 7, delay = 1)
+    init_output(p_pin)
+    GPIO.output(p_pin, GPIO.LOW)
+    time.sleep(1)
+    GPIO.ouput(p_pin, GPIO.HIGH)
