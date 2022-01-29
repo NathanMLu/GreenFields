bbab864fb7a18c7981a968c5505203b9999bc8ff
Nathan Lu <nathanlumax@gmail.com>
2022-01-29 15:23:29 -0600

    test

--- pi/pumpV2.py
+++ pi/pumpV2.py
@@ -7,9 +7,15 @@ def init_output(pin):
     GPIO.setup(pin, GPIO.OUT)
     GPIO.output(pin, GPIO.LOW)
     GPIO.output(pin, GPIO.HIGH)
+    pump_on()
 
-def pump_on(p_pin = 7, delay = 1)
+def pump_on(p_pin = 7, delay = 10):
     init_output(p_pin)
+    print("starting pump")
     GPIO.output(p_pin, GPIO.LOW)
-    time.sleep(1)
+    time.sleep(delay)
     GPIO.ouput(p_pin, GPIO.HIGH)
+    print("stopping pump")
+
+init_output(7)
+
