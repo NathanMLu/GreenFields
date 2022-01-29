eb93029579359742aa051114057da70a1a3fabca
Nathan Lu <nathanlumax@gmail.com>
2022-01-29 16:59:29 -0600

    v3

--- pi/pumpV3.py
+++ pi/pumpV3.py
@@ -2,5 +2,10 @@
 import time
 import RPi.GPIO as GPIO
 
-#GPIO setup ---- Pump = pin 
+#GPIO setup ---- Pump = pin 7, GPIO 4
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7, GPIO.OUT)
+
+GPIO.ouput(7,True)
+
 	
