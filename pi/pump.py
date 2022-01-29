316d1da5294e5f38027ff8f0d3a261ef599e43c9
Nathan Lu <nathanlumax@gmail.com>
2022-01-29 16:36:21 -0600

    added better syntax

--- pi/pump.py
+++ pi/pump.py
@@ -1,32 +1,21 @@
 
-"""
 import RPi.GPIO as GPIO
 import time
 
 
 def setup():
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(7, GPIO.OUT)
-    startPump()
+    pinOn(11)
 
-def startPump():
-    print ("starting pump!")
-    GPIO.output(7,GPIO.LOW)
+
+def pinOn(pin):
+    GPIO.setup(pin, GPIO.OUT)
+    print ("Turning on pin ", pin)
+    GPIO.output(pin, GPIO.HIGH)
     time.sleep(10)
-    GPIO.output(7, GPIO.HIGH)
-    print("stopping pump!")
+    GPIO.output(pin, GPIO.LOW)
+    print("Turning off pin ", pin)
 
     GPIO.cleanup()
-    """
-import RPi.GPIO as GPIO     
-from time import sleep
 
-GPIO.setwarnings(False)            
-GPIO.setmode(GPIO.BOARD)      
-
-GPIO.setup(7, GPIO.OUT)        
-GPIO.output(7, GPIO.LOW)
-GPIO.output(7, GPIO.HIGH)  
-print("running indefinitely")
-
-
+setup()
