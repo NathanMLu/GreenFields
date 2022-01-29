d03f5f8d009367e10da05e90f6b99e5c3d90c330
Nathan Lu <nathanlumax@gmail.com>
2022-01-29 15:08:41 -0600

    trying

--- pi/pump.py
+++ pi/pump.py
@@ -1,3 +1,5 @@
+
+"""
 import RPi.GPIO as GPIO
 import time
 
@@ -15,10 +17,20 @@ def startPump():
     print("stopping pump!")
 
     GPIO.cleanup()
+    """
+import RPi.GPIO as GPIO            # import RPi.GPIO module  
+from time import sleep             # lets us have a delay  
+GPIO.setmode(GPIO.BCM)             # choose BCM or BOARD  
+GPIO.setup(7, GPIO.OUT)           # set GPIO24 as an output   
+  
+try:  
+    while True:  
+        GPIO.output(7, 1)         # set GPIO24 to 1/GPIO.HIGH/True  
+        sleep(0.5)                 # wait half a second  
+        GPIO.output(7, 0)         # set GPIO24 to 0/GPIO.LOW/False  
+        sleep(0.5)                 # wait half a second  
+  
+except KeyboardInterrupt:          # trap a CTRL+C keyboard interrupt  
+    GPIO.cleanup()  
 
 
-
-
-
-
-setup()
