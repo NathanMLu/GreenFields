c43f062814f87e4c2bdc49497561e254129b6743
Nathan Lu <nathanlumax@gmail.com>
2022-01-29 15:45:20 -0600

    fun

--- pi/pump.py
+++ pi/pump.py
@@ -21,13 +21,13 @@ def startPump():
 import RPi.GPIO as GPIO            # import RPi.GPIO module  
 from time import sleep
 GPIO.setwarnings(False)             # lets us have a delay  
-GPIO.setmode(GPIO.BCM)             # choose BCM or BOARD  
-GPIO.setup(4, GPIO.OUT)           # set GPIO24 as an output   
+GPIO.setmode(GPIO.BOARD)             # choose BCM or BOARD  
+GPIO.setup(7, GPIO.OUT)           # set GPIO24 as an output   
   
 print("running")
-GPIO.output(4, 1)         # set GPIO24 to 1/GPIO.HIGH/True  
+GPIO.output(7, 1)         # set GPIO24 to 1/GPIO.HIGH/True  
 sleep(10)                 # wait half a second  
-GPIO.output(4, 0)         # set GPIO24 to 0/GPIO.LOW/False  
+GPIO.output(7, 0)         # set GPIO24 to 0/GPIO.LOW/False  
 print("stopping")
 GPIO.cleanup()  
 
