d585740c67275f13b74f075457329cf243e43fcc
Nathan Lu <nathanlumax@gmail.com>
2022-01-29 14:55:38 -0600

    t

--- pi/pump.py
+++ pi/pump.py
@@ -3,14 +3,16 @@ import time
 
 def startPump():
 
-    GPIO.setmode(GPIO.BCM)
+    GPIO.setmode(GPIO.BOARD)
     print ("starting pump!")
 
     GPIO.setup(7, GPIO.OUT)
 
+    GPIO.output(7, GPIO.LOW)
+
     GPIO.output(7, GPIO.HIGH)
     time.sleep(10)
-
+    GPIO.output(7, GPIO.LOW)
     print("stopping pump!")
     GPIO.output(7, GPIO.LOW)
 
