75456a65d7af19827bad980985d82d8d68394725
Nathan Lu <nathanlumax@gmail.com>
2022-01-29 15:02:34 -0600

    testing

--- pi/pump.py
+++ pi/pump.py
@@ -1,20 +1,21 @@
 import RPi.GPIO as GPIO
 import time
 
+
 def setup():
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(7, GPIO.OUT)
     startPump()
 
 def startPump():
-
-    GPIO.output(7,0)
-    time.sleep(3)
     print ("starting pump!")
-    GPIO.output(7, 1)
+    GPIO.output(7,GPIO.LOW)
     time.sleep(10)
-    GPIO.output(7, 0)
+    GPIO.output(7, GPIO.HIGH)
     print("stopping pump!")
+
+    GPIO.cleanup()
+
 
 
 
