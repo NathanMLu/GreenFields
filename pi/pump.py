31306ae789669ee07c7956d1e7828da93b9c1886
Nathan Lu <nathanlumax@gmail.com>
2022-01-29 14:43:50 -0600

    updated pump pin number

--- pi/pump.py
+++ pi/pump.py
@@ -6,11 +6,11 @@ def startPump():
     GPIO.setmode(GPIO.BCM)
     print ("starting pump!")
 
-    GPIO.output(4, GPIO.HIGH)
+    GPIO.output(7, GPIO.HIGH)
     time.sleep(10)
 
     print("stopping pump!")
-    GPIO.output(4, GPIO.LOW)
+    GPIO.output(7, GPIO.LOW)
 
 
 startPump()
