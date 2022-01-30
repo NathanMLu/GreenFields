0ef346f3b99a41606675625edc096478c7f8b8d5
Nathan Lu <nathanlumax@gmail.com>
2022-01-29 21:26:23 -0600

    more test

--- pi/pumpRunner.py
+++ pi/pumpRunner.py
@@ -4,15 +4,15 @@ import RPi.GPIO as GPIO
 
 pin = 40
 
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BOARD) #setting boardset
 GPIO.setup(pin, GPIO.OUT)
 
 print("Turning pin on")    
 GPIO.output(pin, 1)
-time.sleep(25)
-GPIO.output(pin, 0)
-print("Turning pin off")
+#time.sleep(25)
+#GPIO.output(pin, 0)
+#print("Turning pin off")
 
-GPIO.cleanup()  
+#GPIO.cleanup()  
 
 	
