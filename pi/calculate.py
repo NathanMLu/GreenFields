8ffccff85b65807bf325f983c868320470c0d6d3
Nathan Lu <nathanlumax@gmail.com>
2022-01-30 11:37:43 -0600

    added score calculator

--- pi/calculate.py
+++ pi/calculate.py
@@ -1,10 +1,14 @@
 score = 100
-"""
+
 from pumpHandler import *
 from sensorHandler import *
-"""
 
 def getScore():
     global score
+
+    if not hasWater:
+        score = 75
+    else:
+        score = 100
 
     return score
