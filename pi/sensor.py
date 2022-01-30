14b67a2f1e9bf012adff45247fdc7770ae40c2c9
Nathan Lu <nathanlumax@gmail.com>
2022-01-29 23:02:57 -0600

    sensor

--- pi/sensor.py
+++ pi/sensor.py
@@ -6,6 +6,6 @@ channel = 17
 GPIO.setup(channel, GPIO.IN)
 GPIO.add_event_detect(channel, GPIO.BOTH)
 ouncetime =300
-GPIO.add_event_callbacl(channel, callback)
+GPIO.add_event_callback(channel, callback)
 while True:
     time.sleep(0.1)
