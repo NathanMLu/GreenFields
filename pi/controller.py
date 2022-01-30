64132ed7fc3d83dbeda941efc8516f64ad89f692
Nathan Lu <nathanlumax@gmail.com>
2022-01-30 08:23:39 -0600

    has water

--- pi/controller.py
+++ pi/controller.py
@@ -3,8 +3,10 @@ from pumpHandler import *
 from pumpStopper import *
 
 while True:
-        
+    time.sleep(0.5)
     if hasWater:
+        print("pumping")
         pump()
     else:
+        print("stopping")
         stop()
