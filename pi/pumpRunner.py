7f2a9f9d0dff758707715545eb3dc2b27ba77992
Nathan Lu <nathanlumax@gmail.com>
2022-01-29 22:54:03 -0600

    test

--- pi/pumpRunner.py
+++ pi/pumpRunner.py
@@ -1,18 +1,18 @@
 import time
-import keyboard
 import RPi.GPIO as GPIO
 
-pin = 40
+pin = 18
 
-GPIO.setmode(GPIO.BOARD) #setting boardset
+GPIO.setmode(GPIO.BCM) #setting boardset
 GPIO.setup(pin, GPIO.OUT)
 
-print("Turning pin on")    
-GPIO.output(pin, 1)
-#time.sleep(25)
-#GPIO.output(pin, 0)
-#print("Turning pin off")
 
-#GPIO.cleanup()  
+while (True):
+ 
+    GPIO.output(pin, 1)
+    time.sleep(1)
+    GPIO.output(pin, 0)
+    time.sleep(1)
+
 
 	
---
+++ pi/sensor.py
@@ -0,0 +1,11 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+channel = 17
+GPIO.setup(channel, GPIO.IN)
+GPIO.add_event_detect(channel, GPIO.BOTH)
+ouncetime =300
+GPIO.add_event_callbacl(channel, callback)
+while True:
+    time.sleep(0.1)
