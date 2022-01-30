2acf5a11fde2e392e7272b3e7a88df55360a7ede
Nathan Lu <nathanlumax@gmail.com>
2022-01-30 10:16:44 -0600

    test

--- pi/reader.py
+++ pi/reader.py
@@ -1,8 +1,11 @@
+from tracemalloc import start
 import requests
 import RPi.GPIO as GPIO
 import threading
+import time
 import json
 
+from pumpHandler import *
 from sensorHandler import hasWater
 from calculate import getScore
         
@@ -15,3 +18,14 @@ result = {"dampness": damp, "score": score, "temp": temp}
 print(result)
 
 res = requests.post('https://aqueous-tor-90407.herokuapp.com/data', json=result)
+
+def search():
+    while True:
+        time.sleep(2)
+        res = requests.get('https://aqueous-tor-90407.herokuapp.com/water')
+        if (res):
+            startPumping()
+
+t1 = threading.Thread(target = search)
+t1.start()
+t1.join()
