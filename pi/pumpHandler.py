75f70b9de4176533bc330df2e90d5cea9e1d9e28
Nathan Lu <nathanlumax@gmail.com>
2022-01-30 01:40:05 -0600

    reformat

--- pi/pumpHandler.py
+++ pi/pumpHandler.py
@@ -6,23 +6,21 @@ PUMPING = False
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(PIN, GPIO.OUT)
-"""
+
 def pump():
     global PUMPING, PIN
 
     if not PUMPING:
+        GPIO.output(PIN, 0)
+        time.sleep(10)
         GPIO.output(PIN, 1)
         time.sleep(10)
-        GPIO.output(PIN, 0)
+        
         print("pumping :))))")
 
     PUMPING = False
-"""    
+  
 
 while True:
-    GPIO.output(PIN, 1)
-    time.sleep(10)
-    GPIO.output(PIN, 0)
-    time.sleep(10)
-    print("pumping :))))")
+    pump()
 
