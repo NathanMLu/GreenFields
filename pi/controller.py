5c6fd3cb29bf372b70cfe2fa7da4b54554e63c6a
Nathan Lu <nathanlumax@gmail.com>
2022-01-30 09:10:26 -0600

    threading

--- pi/controller.py
+++ pi/controller.py
@@ -1,18 +1,78 @@
+import threading
+import RPi.GPIO as GPIO
+import time
+
 from sensorHandler import *
-"""
 from sensorHandler import *
 from pumpHandler import *
 from pumpStopper import *
 
-print("starting program")
+GPIO.setmode(GPIO.BOARD)
 
-while True:
-    print("Starting pump")
+""" MOISTURE SENSOR """
 
-    startPumping()
+soilPin = 11
+water = False
+
+GPIO.setup(soilPin, GPIO.IN)
+
+def callback(soilPin):
+    global water
+
+    if not GPIO.input(soilPin):
+        water = False
+        print("No water detected")
+    else:
+        water = True
+        print("Water detected")
+
+GPIO.add_event_detect(soilPin, GPIO.BOTH, bouncetime=300)
+GPIO.add_event_callback(soilPin, callback)
+
+def sensorLoop():
+    while True:
+        time.sleep(1)
+
+
+
+""" PUMP """
+
+PIN = 18
+GPIO.setup(PIN, GPIO.OUT)
+
+def startPumping():
+    print("pumping")
+    global PIN
+    GPIO.output(PIN, 0)
     time.sleep(5)
-    print("Stopping pump")
-    stopPumping
+
+
+def stopPumping():
+    print("stopping")
+    global PIN
+    GPIO.output(PIN, 1)
     time.sleep(5)
-    """
-print("Hello World")
+
+def pumpLoop():
+    while True:
+        print("Starting pump")
+        startPumping()
+        time.sleep(5)
+        print("Stopping pump")
+        stopPumping
+        time.sleep(5)
+
+# Threading  
+
+t1 = threading.Thread(target = sensorLoop)
+t2 = threading.Thread(target = pumpLoop)
+t1.start()
+t2.start()
+t1.join()
+t2.join()
+        
+
+
+
+
+
